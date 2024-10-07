f4fd0e372fa42617aac2877753d0bf38906a3e84
kamranation <kamranation@outlook.com>
2024-10-08 03:11:47 +0400

    Added: api folder

--- api/main.js
+++ api/main.js
@@ -1,3 +1,5 @@
+// api/bot.js
+
 // Suppress only the punycode deprecation warning
 const originalEmitWarning = process.emitWarning;
 process.emitWarning = (warning, type, ...args) => {
@@ -7,16 +9,13 @@ process.emitWarning = (warning, type, ...args) => {
   return originalEmitWarning(warning, type, ...args);
 };
 
-// Now the rest of your bot code
 const { Telegraf } = require("telegraf");
-const { db } = require("./firebaseConfig");
+const { db } = require("../firebaseConfig");
 
-// Replace 'YOUR_BOT_TOKEN' with the actual token from BotFather
-const bot = new Telegraf("7807368465:AAEJhDrJHe3RDPtm1m2_XVwewPEwmkI3_qs");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Command to save referral data
 bot.start((ctx) => {
-  const messageText = ctx.message.text || ""; // Ensure message text is defined
+  const messageText = ctx.message.text || ""; 
   const referrerId = messageText.includes("ref_") ? messageText.split(" ")[1]?.replace("ref_", "") : null;
   const userId = ctx.from.id.toString();
   if (referrerId) {
@@ -30,17 +29,17 @@ bot.start((ctx) => {
     //   console.error("Error saving referral data:", error);
     //   ctx.reply("Xəta baş vermişdir (:");
     // });
-    ctx.reply(ctx);
   } else {
     ctx.reply("Bota xoş gəlmisiniz! Cədvəlinizi paylaşmaq üçün /share_schedule istifadə edin.");
   }
 });
 
-// Command to generate a share link
 bot.command("share_schedule", (ctx) => {
   const referrerId = ctx.from.id;
   const miniAppLink = `https://t.me/tg_scheduler_bot/tg_scheduler_app?start=ref_${referrerId}`;
   ctx.reply(`Bu linkdən istifadə edərək cədvəlinizi paylaşın: ${miniAppLink}`);
 });
 
-bot.launch();
+module.exports = async (req, res) => {
+  await bot.handleUpdate(req.body, res);
+};
