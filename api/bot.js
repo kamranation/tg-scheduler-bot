c80c2f930d59beb512c7ef54a81168cc3ec4c59e
kamranation <kamranation@outlook.com>
2024-10-16 23:36:35 +0400

    Fixed: reff if

--- api/bot.js
+++ api/bot.js
@@ -19,11 +19,24 @@ bot.start((ctx) => {
   const referrerId = messageText.includes("ref_") ? messageText.split(" ")[1]?.replace("ref_", "") : null;
   const userId = ctx.from.id.toString();
   
-  const startParam = referrerId ? `ref_${referrerId}` : `ref_${userId}`;
-  const miniAppLink = `https://t.me/tg_scheduler_bot/tg_scheduler_app?start=${startParam}`;
-  ctx.reply(referrerId);
+  // const startParam = referrerId ? `ref_${referrerId}` : `ref_${userId}`;
+  // const miniAppLink = `https://t.me/tg_scheduler_bot/tg_scheduler_app?start=${startParam}`;
+
+  // ctx.reply("Bota xoş gəlmisən! Cədvəlini paylaşmaq üçün /share_schedule kliklə.");
+  
   if (referrerId) {
-    ctx.reply(`Sizi dəvət edənin linki: ${miniAppLink}`);
+    ctx.reply("Səni dəvət edənin cədvəlinə keçid.", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Cədvələ keçid",
+              url: `https://t.me/tg_scheduler_bot/tg_scheduler_app?start=ref_${referrerId}`
+            }
+          ]
+        ]
+      }
+    });
   } else {
     ctx.reply("Bota xoş gəlmisən! Cədvəlini paylaşmaq üçün /share_schedule kliklə.", {
       reply_markup: {
@@ -31,7 +44,7 @@ bot.start((ctx) => {
           [
             {
               text: "Cədvələ keçid",
-              url: miniAppLink
+              url: `https://t.me/tg_scheduler_bot/tg_scheduler_app?start=ref_${userId}`
             }
           ]
         ]
